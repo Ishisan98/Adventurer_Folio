b24ce9eeca81889a381c5e9b8295a3583c8f7707
Wathanta Aung <wathanta_aung@eaim.edu.sg>
2024-06-29 15:07:55 +0800

    initial commit

---
+++ src/components/Introduction.js
@@ -0,0 +1,32 @@
+import {React} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../src/App.css';
+
+function Introduction() {
+    return (
+        <div className='row'>
+            <div className="float-sm-start col-sm-5 col-12 vertical-left-center-column">
+                <div className="d-flex flex-row pl-lg">
+                    <h1 className="custom-title-xlg white">WATHANTA</h1>  
+                    <h1 className="custom-title-xlg orange pl-sm">AUNG</h1>
+                </div>
+                <div className="d-flex flex-row pl-lg">
+                    <h1 className="custom-title-lg white">SOFTWARE</h1>  
+                    <h1 className="custom-title-lg orange pl-sm">DEVELOPER</h1>
+                </div>
+                <br></br><br></br>
+                <div className="d-flex flex-row pl-lg pr-lg pb-lg">
+                    <p className="white"> 
+                        An enthusiastic Software Developer with 2 years of experience, passionate about solving everyday-life problems and business challenges through creative and dedicated development of Applications, Websites, and Web-based Systems.
+                    </p>
+                </div>
+            </div>
+            <div className="float-sm-start col-sm-1 col-12"></div>
+            <div className="float-sm-end col-sm-6 col-12 vertical-center-column">
+                <img src={require('../../src/images/character.png')} className="character-img pr-lg" alt="character-img" />
+            </div>
+        </div>
+    )
+}
+
+export default Introduction;
